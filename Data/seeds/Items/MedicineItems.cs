aab958445d5417ef67aae66a2295a41aab8025f8
Ignacio Angel <Ignacio Angel>
2026-08-25 21:36:17 -0400

    chore: seed 200 dummy products across all categories

---
+++ Data/seeds/Items/MedicineItems.cs
@@ -0,0 +1,22 @@
+namespace vet_api_Net.Data.seeds.Items;
+
+public static class MedicineItems
+{
+    public static readonly string[] Nombres =
+    {
+        "Amoxicilina 250mg", "Amoxicilina 500mg", "Cefalexina 250mg", "Enrofloxacina 50mg",
+        "Enrofloxacina 150mg", "Metronidazol 250mg", "Doxiciclina 100mg", "Meloxicam 1.5mg/ml",
+        "Carprofeno 25mg", "Carprofeno 75mg", "Prednisona 5mg", "Prednisolona 20mg",
+        "Dexametasona Inyectable", "Furosemida 40mg", "Enalapril 5mg", "Omeprazol 20mg",
+        "Ranitidina 150mg", "Maropitant Antiemético", "Tramadol 50mg", "Gabapentina 100mg",
+        "Fenobarbital 30mg", "Insulina Veterinaria", "Vitamina B12 Inyectable", "Complejo B Inyectable",
+        "Hierro Dextrano Inyectable", "Suero Fisiológico 500ml", "Suero Ringer Lactato 500ml",
+        "Dextrosa 5% 500ml", "Desparasitante Interno Cachorro", "Desparasitante Interno Adulto",
+        "Desparasitante Externo Pipeta", "Antipulgas Collar", "Antipulgas Spray", "Antigarrapatas Spray",
+        "Suplemento Articular Glucosamina", "Suplemento Condroprotector", "Probiótico Digestivo",
+        "Enzimas Digestivas", "Loción Ótica Antibiótica", "Colirio Oftálmico Antibiótico",
+        "Pomada Cicatrizante", "Antiséptico Clorhexidina", "Povidona Yodada", "Alcohol Antiséptico 70%",
+        "Jeringa Descartable 3ml", "Jeringa Descartable 5ml", "Aguja Hipodérmica 21G",
+        "Gasas Estériles", "Venda Elástica", "Guantes de Nitrilo Caja"
+    };
+}
